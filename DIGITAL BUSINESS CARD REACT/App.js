c1772ce68d67d3887876e8c1f8e0372517c81f39
danielferguson <107422624+danielalexis1860@users.noreply.github.com>
2024-03-13 03:51:51 -0700

    Add files via upload

---
+++ DIGITAL BUSINESS CARD REACT/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Navbar from "./COMPONENTS/Navbar"
+import Hero from "./COMPONENTS/Hero"
+
+export default function App(){
+    return(
+        <div className = "">
+          <Navbar />
+          <Hero />
+        </div>
+    )
+}
+
